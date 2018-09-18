0cfea601d60a2d53e748545690586b4a3f293cf6
unknown <qiangdong8825@gmail.com>
2018-09-18 07:11:48 -0700

    test update

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,7 +2,7 @@
 $targetBlank = isset($mTargetBlank) ? ' target="_blank"' : '';
 ?>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Sketch Maker PRO v1.0</a>
+  <a class="navbar-brand" href="#">Sketch Maker PRO v1.0 - test</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
           aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
